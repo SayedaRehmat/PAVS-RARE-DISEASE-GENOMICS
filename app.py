e5baa250f0662040c34ed160e55d735cfc20a247
SayedaRehmat <bc190408541@vu.edu.pk>
2026-05-06 21:11:49 +0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 # SAFE PATH HANDLING
 # ─────────────────────────────
 sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
-from src.analysis import run_for_dashboard
+from analysis import run_for_dashboard
 
 # ─────────────────────────────
 # CONFIG
